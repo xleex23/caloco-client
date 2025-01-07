19fed991e5f32ea976f22816f37e242b54e5807c
xleex23 <jameswlee109@gmail.com>
2025-01-07 12:54:36 -0600

    ui and basic calculations

--- src/_components/home-counter/home-counter.component.ts
+++ src/_components/home-counter/home-counter.component.ts
@@ -1,19 +1,56 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { IntegerDirective } from 'src/_directives/integer.directive';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home-counter',
   templateUrl: './home-counter.component.html',
   styleUrls: ['./home-counter.component.scss'],
-  imports: [CommonModule]
+  imports: [CommonModule, IntegerDirective, ReactiveFormsModule]
 })
 export class HomeCounterComponent {
-    public active_tab: string = 'add';
-    public current_total: number = 0;
+    public integer_input: FormControl = new FormControl();
+    public active_count_tab: string = 'add';
+    public active_macro_tab: string = 'cals';
+    public current_cal: number = 2399;
+    public current_pro: number = 189;
+    private validCombinations = {
+        add: {
+          cals: () => {
+            this.current_cal += this.integer_input.value;
+          },
+          pro: () => {
+            this.current_pro += this.integer_input.value;
+          },
+        },
+        subtract: {
+          cals: () => {
+            this.current_cal -= this.integer_input.value;
+          },
+          pro: () => {
+            this.current_pro -= this.integer_input.value;
+          },
+        }
+    };
 
     constructor() {}
 
-    toggleTab(tab: string) {
-        this.active_tab = tab;
+    toggleTab(tabType: 'count' | 'macro', value: string) {
+        if (tabType === 'count') {
+            this.active_count_tab = value;
+        } else if (tabType === 'macro') {
+            this.active_macro_tab = value;
+        }
+    }
+
+    update() {
+        if (!this.integer_input.value) {
+            return
+        }
+        const countTab = this.active_count_tab;
+        const macroTab = this.active_macro_tab;
+        this.validCombinations[countTab]?.[macroTab]?.();
+        this.integer_input.setValue(null)
     }
 }
