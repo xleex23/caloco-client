9a17cf1138778cb04f97de215646e22a6d1c06d3
James Lee <jlee@bleachr.co>
2025-01-20 16:20:36 -0600

    revert stub

--- src/_components/home-counter/home-counter.component.ts
+++ src/_components/home-counter/home-counter.component.ts
@@ -8,12 +8,5 @@ import { Component } from '@angular/core';
   imports: [CommonModule]
 })
 export class HomeCounterComponent {
-    public active_tab: string = 'add';
-    public current_total: number = 0;
-
-    constructor() {}
-
-    toggleTab(tab: string) {
-        this.active_tab = tab;
-    }
+    
 }
