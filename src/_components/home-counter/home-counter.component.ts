6083a0bd70dbe42c46f9476651e39712f64818b2
James Lee <jlee@bleachr.co>
2025-01-22 08:31:39 -0600

    min age

--- src/_components/home-counter/home-counter.component.ts
+++ src/_components/home-counter/home-counter.component.ts
@@ -13,9 +13,10 @@ export class HomeCounterComponent {
     public someForm: FormGroup = this.fb.group({
         name: ['', [Validators.required, Validators.minLength(2)]],
         email: ['', [Validators.required, Validators.email]],
-        phone_number: ['', [Validators.required, Validators.pattern(/^\+?\d{10,15}$/)]],
-        student_id: ['', Validators.required, Validators.pattern(/^\d+$/)],
-        password: ['', Validators.required, Validators.minLength(8), this.passwordValidator()]
+        phoneNumber: ['', [Validators.required, Validators.pattern(/^\+?\d{10,15}$/)]],
+        studentId: ['', Validators.required, Validators.pattern(/^\d+$/)],
+        password: ['', Validators.required, Validators.minLength(8), this.passwordValidator()],
+        birthdate: [new Date().toISOString(), Validators.required, this.minimumAgeValidator(18)]
     })
     private data: any[] = [];
     private errorMessage: string = '';
@@ -98,4 +99,18 @@ export class HomeCounterComponent {
             return isValid ? null : { passwordStrength: true };
         }
     }
+
+    minimumAgeValidator(minAge: number) {
+        return (control: { value: string | null }) => {
+            if (!control.value) return null;
+            
+            const birthDate = new Date(control.value);
+            const age = Math.floor((Date.now() - birthDate.getTime()) / (1000 * 60 * 60 * 24 * 365.25)); // Calculate the age.
+            
+            // If the calculated age is greater than or equal to the minimum age, return null (valid).
+            // Else, return an error with the minimum age and the calculated age.
+            return age >= minAge ? null : { minimumAge: { requiredAge: minAge, actualAge: age } };
+        };
+      }
+      
 }
